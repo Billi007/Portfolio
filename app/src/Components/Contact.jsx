0e7d431a8c993cf720f01150c9c7a6082ea5d38b
Tanishka <guptatanishka2008@gmail.com>
2025-02-12 14:30:30 +0530

    add file

--- app/src/Components/Contact.jsx
+++ app/src/Components/Contact.jsx
@@ -1,4 +1,3 @@
-import {Link} from 'react-router-dom'
 import { GiCoffeeCup } from "react-icons/gi";
 
 const Contact = () => {
--- app/src/Components/Resume_section.jsx
+++ app/src/Components/Resume_section.jsx
@@ -4,7 +4,7 @@ const ResumeSection = () => {
   return (
     <div className="flex justify-center items-center mt-6">
       <a
-        href="https://drive.google.com/file/d/1pWqwpvDfRQXu98gv81aea44j_0cu-ZVi/view?usp=sharing"
+        href="https://drive.google.com/file/d/1zTbn6zIPh5G79d0Vk4rM-y2umwhdH14s/view?usp=sharing"
         download="My_Resume.pdf"
         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center"
       >
